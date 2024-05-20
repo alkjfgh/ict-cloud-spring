87da46d7708c6dd878bdf11f0fd4ca76756f3219
LHC <dlgmlckd02@naver.com>
2024-05-20 16:27:23 +0900

    -account.css- -account.js- -account.jsp- -user.css- -user.jsp- id및 클래스 수정

--- src/main/webapp/WEB-INF/user/user.jsp
+++ src/main/webapp/WEB-INF/user/user.jsp
@@ -1,40 +1,35 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: lhc99
-  Date: 24. 5. 10.
-  Time: 오전 8:59
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
+    <script src="../../js/account.js"></script>
+    <link rel="stylesheet" href="../../css.user.css?ver=1">
     <title>user</title>
 </head>
 <body>
-    <div>
+<div class="user-box">
 
-        <div class="user-text">
-            <h3>당신의 계정</h3>
-        </div>
-
-        <form action="account.jsp">
-            <div class="logout">
-                <input type="submit" id="logout-button" value="로그아웃">
-            </div>
-        </form>
-
-        <form action="">
-            <div class="advanced-setting">
-                <input type="submit" id="setting-button" value="고급 설정">
-            </div>
-        </form>
-
-        <form action="deleteaccount.jsp">
-            <div class="delete-user">
-                <input type="submit" id="delete-button" value="계정 삭제">
-            </div>
-        </form>
-
+    <div class="user-text">
+        <h3>Your account</h3>
     </div>
+
+    <form action="account.jsp">
+        <div class="logout">
+            <input type="submit" id="logout-button" value="LogOut">
+        </div>
+    </form>
+
+    <form action="">
+        <div class="advanced-setting">
+            <input type="submit" id="setting-button" value="Settings">
+        </div>
+    </form>
+
+    <form action="deleteaccount.jsp">
+        <div class="delete-user">
+            <input type="submit" id="delete-button" value="Delete Account" onclick="deleteaccount()">
+        </div>
+    </form>
+
+</div>
 </body>
 </html>
--- src/main/webapp/js/account.js
+++ src/main/webapp/js/account.js
@@ -1,33 +1,65 @@
-$(document).ready(() => {
-    $('#signupButton').click(function () { //회원가입 버튼을 누르면
-        $('.login-box').hide() //로그인 박스를 숨김
-        $('.signup-box').show() //회원가입 버튼을 보여줌
-        $('#loginButton').show() //로그인 버튼을 보여줌
-    });
+// $('.logIn-form').submit(function (event) {
+const submitForm = (event) => {
+    event.preventDefault(); // 기본 폼 제출 동작을 막습니다.
 
-    $('#loginButton').click(() => { //로그인 버튼을 누르면
-        $('.signup-box').hide() //회원가입 박스를 숨김
-        $('#loginButton').hide() //로그인 버튼을 숨김
-        $('.login-box').show() //로그인 박스를 보여줌
-    });
+    alert("logIn-form");
 
-    $('#signIn-form').submit(function (event) {
-        event.preventDefault(); // 기본 폼 제출 동작을 막습니다.
-
-        $.ajax({
-            url: '/user/signIn',
-            type: 'POST',
-            data: $(this).serialize(), // 폼 데이터를 직렬화합니다.
-            success: function (response) {
-                if (response.status === 'success') {
-                    window.location.href = '../file/upload'; // 로그인 성공 시 페이지 이동
-                } else {
-                    alert(response.message); // 로그인 실패 시 메시지 표시
-                }
-            },
-            error: function (xhr, status, error) {
-                alert('로그인 요청 중 오류가 발생했습니다.');
+    $.ajax({
+        url: '/user/signIn',
+        type: 'POST',
+        data: $('.login-form').serialize(), // 폼 데이터를 직렬화합니다.
+        success: function (response) {
+            if (response.status === 'success') {
+                window.location.href = '../file/upload'; // 로그인 성공 시 페이지 이동
+            } else {
+                alert(response.message); // 로그인 실패 시 메시지 표시
             }
-        });
+        },
+        error: function (xhr, status, error) {
+            alert('An error occurred during the login request.');
+        }
+    });
+}
+
+
+const signUpSubmit = async (event) => {
+    event.preventDefault(); //submit을 막음
+
+    let name = $(".input-signup-name").val();
+    let email = $(".input-signup-id").val();
+    let password = $(".input-signup-pwd").val();
+
+    //이름 영어만 입력 가능하도록
+    let eng = /^[a-zA-Z]*$/;
+    if(!eng.test(name)){
+        alert("Please enter your name in English only.")
+        return false;
+    }
+
+    // 비밀번호 검사
+    let checkpwd = $(".input-signup-pwd-check").val();
+    if(password !== checkpwd){
+        alert("The passwords don't match.");
+        return false;
+    }
+
+    // 이메일 정규식 검사
+    let email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+    if(!email_regex.test(email)){
+        alert("Please format your email accordingly.")
+        return false;
+    }
+
+    let passwd_regex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,16}$/;
+    if(!passwd_regex.test(password)){
+        alert("Please enter the password in the correct format.")
+        return false;
+    }
+    return true;
+}
+
+$(document).ready(function(){
+    $('.message a').click(function(){
+        $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
     });
 });
