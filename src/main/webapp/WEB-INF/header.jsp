bd090d95353a8638b1e7079c07289da74b872ace
alkjf <alkjfgh@naver.com>
2024-05-27 14:16:36 +0900

    사이드바 수정, 헤더 추가

---
+++ src/main/webapp/WEB-INF/header.jsp
@@ -0,0 +1,13 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<link rel="stylesheet" href="<c:url value='/css/header.css?ver=1'/>"/>
+
+<script src="<c:url value='/js/header.js?ver=1'/>"></script>
+
+<div class="header">
+    <div class="sidebar-open">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
+    </div>
+</div>
---
+++ src/main/webapp/js/header.js
@@ -0,0 +1,19 @@
+let isSidebarOpen = false
+$(document).ready(function () {
+    $('.sidebar-open').on('click', function () {
+        // $('.sidebar').show();
+        $('.sidebar').css('left', '0'); // 사이드바를 오른쪽으로 이동
+        isSidebarOpen = true;
+    });
+
+    $(document).on('click', function () {
+        if(isSidebarOpen){
+            const sidebar = $(".sidebar");
+            const sidebar_open = $(".sidebar-open");
+            if (!sidebar_open.is(event.target) && !sidebar_open.has(event.target).length && !sidebar.is(event.target) && !sidebar.has(event.target).length) {
+                // sidebar.hide();
+                sidebar.css('left', '-250px'); // 사이드바를 왼쪽으로 이동
+            }
+        }
+    });
+});
