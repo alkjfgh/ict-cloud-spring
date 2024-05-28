083290feac3c07b036029d8fcb3725a363cf4893
LHC <dlgmlckd02@naver.com>
2024-05-28 13:27:46 +0900

    -header.jsp- -header.scss- -header.js- dropdown 메뉴 구현

--- src/main/webapp/js/header.js
+++ src/main/webapp/js/header.js
@@ -17,3 +17,18 @@ $(document).ready(function () {
         }
     });
 });
+
+let dropdownMenu = document.querySelector(".dropdown-menu")
+let dropdownButton = document.querySelector(".dropdown-button")
+
+dropdownButton.addEventListener("click", function(event) {
+    if (this.active) {
+        dropdownMenu.classList.remove("active")
+    } else {
+        dropdownMenu.classList.add("active")
+    }
+
+    this.active = !this.active
+})
+
+dropdownButton.active = false
