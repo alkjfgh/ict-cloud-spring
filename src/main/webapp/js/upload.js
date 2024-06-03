a28d096d596dba52e51e2c7cb6fd3e3ffd13074a
alkjf <alkjfgh@naver.com>
2024-06-03 15:00:16 +0900

    공지사항 이상하게 줄 나오던거 수정

--- src/main/webapp/js/upload.js
+++ src/main/webapp/js/upload.js
@@ -524,8 +524,6 @@ const enterFolder = async (p) => {
         addFile.children.item(3).value = data.p;
 
         const addFolder = document.getElementById('addFolder');
-        console.log(storagePath)
-        console.log(String.raw`${data.storagePath}`)
         addFolder.onclick = () => addFolderHandler(data.userID, data.p, String.raw`${data.storagePath}`);
 
         const fileListElement = document.getElementsByClassName('file-list-table')[0];
